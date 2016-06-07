bca8e3701fa21d94ed723e1ba76a708bfa048aa0
tareq021 <tareq021@gmail.com>
2016-06-07 00:23:33 +0000

    Organized the sensor code.

--- sensor.js
+++ sensor.js
@@ -1,9 +1,11 @@
+// Required Modules
 var GPSSensor = require('jsupm_ublox6');
-var myGPSSensor = new GPSSensor.Ublox6(0);
-// var nmea = require('node-nmea');
-nmea = require('nmea-0183')
+var nmea = require('nmea-0183')
 var stringSearcher = require('string-search');
-var GPGGA_VALUE = "";
+
+var myGPSSensor = new GPSSensor.Ublox6(0);
+var variable_value = "";
+var variable_name = 'GPGGA'; // GPGGA, GPRMC, GPGSA, GPGSV, GPVTG
 
 if (!myGPSSensor.setupTty(GPSSensor.int_B9600)) {
 	console.log("Failed to setup tty port parameters");
@@ -17,18 +19,19 @@ function getGPSInfo() {
 	if (myGPSSensor.dataAvailable()) {
 		var rv = myGPSSensor.readData(nmeaBuffer, bufferLength);
 
-		var GPSData, dataCharCode, isNewLine, lastNewLine;
+		var GPSData;
+
 		var numlines = 0;
 		if (rv > 0) {
 			GPSData = "";
 			for (var x = 0; x < rv; x++) {
 				GPSData += nmeaBuffer.getitem(x);
 			}
-			stringSearcher.find(GPSData, 'GPGGA').then(function (resultArr) {
-				if (GPSData.indexOf('$GPGGA') > -1) {
-					GPGGA_VALUE = nmea.parse(resultArr[0].text);
-					// console.log(JSON.stringify(GPGGA_VALUE, null, 2));
-					console.log(GPGGA_VALUE.id);
+			stringSearcher.find(GPSData, variable_name).then(function (resultArr) {
+				if (GPSData.indexOf('$' + variable_name) > -1) {
+					variable_value = nmea.parse(resultArr[0].text);
+					console.log(JSON.stringify(variable_value, null, 2));
+					// console.log(variable_value.id);
 				}
 			});
 		}
