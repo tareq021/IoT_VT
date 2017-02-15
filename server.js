43b85f6874cc4ffabc4c5e946af1719c9738971e
User-24 <user-24@IoT-datasoft.com>
2017-02-15 14:49:27 +0700

    Working Good. Server sending data to mLab (mongoDb).

--- server.js
+++ server.js
@@ -1,38 +1,67 @@
 const mqtt = require('mqtt');
+const request = require('request');
+const express = require("express");
+const bodyParser = require('body-parser');
+const mongodb = require("mongodb");
+const path = require("path");
+const mongoose = require('mongoose');
+// var ObjectID = mongodb.ObjectID;
 var broker = 'mqtt://test.mosquitto.org';
 //var broker='mqtt://broker.hivemq.com';
-const client = mqtt.connect(broker);
-
-var sensorData = "";
-var dataToDb = ""
 
 // MQTT Subscribe
-client.subscribe('GPSData')
-
+const client = mqtt.connect(broker);
+client.subscribe('GPSData');
 client.on('message', function (topic, dataFromSensor) {
 
-    sensorData = JSON.parse(dataFromSensor);
-    dataToDb = processData(sensorData);
-    console.log(JSON.stringify(dataToDb, null, 2));
-    // console.log(JSON.stringify(sensorData, null, 2));
-    // console.log(JSON.stringify(sensorData, null, 2));
+    var sensorData = JSON.parse(dataFromSensor);
+    saveToDb(sensorData);
 });
 
-function processData(sensorData) {
+//Database operation starts here
+mongoose.connect('mongodb://vtdata:vtdata@ds147069.mlab.com:47069/vt');
 
-    var finalData = {
+var dataBaseSchema = new mongoose.Schema({
 
-            "deviceID": sensorData.GPGGA.sensorId,
-            "deviceTime": sensorData.GPGGA.time,
-            "deviceDate": sensorData.GPGGA.date,
-            "latitude": sensorData.GPGGA.latitude,
-            "longitude": sensorData.GPGGA.longitude,
-            "fix": sensorData.GPGGA.fix,
-            "satellites": sensorData.GPGGA.satellites,
-            "altitude": sensorData.GPGGA.altitude,
-            "deviceSpeed": sensorData.GPRMC.speed,
-            "devicecourse": sensorData.GPRMC.course,
-            "deviceVariation": sensorData.GPRMC.variation,
-    }
-    return finalData;
+    deviceID: String,
+    deviceTime: String,
+    deviceDate: String,
+    latitude: String,
+    longitude: String,
+    fix: Number,
+    satellites: Number,
+    altitude: Number,
+    deviceSpeed: Number,
+    devicecourse: Number,
+    deviceVariation: Number
+
+});
+
+var gpsData = mongoose.model('gpsData', dataBaseSchema);
+
+function saveToDb(sensorData) {
+
+    var saveToDb = new gpsData({
+        
+        deviceID: sensorData.GPGGA.sensorId,
+        deviceTime: sensorData.GPGGA.time,
+        deviceDate: sensorData.GPGGA.date,
+        latitude: sensorData.GPGGA.latitude,
+        longitude: sensorData.GPGGA.longitude,
+        fix: sensorData.GPGGA.fix,
+        satellites: sensorData.GPGGA.satellites,
+        altitude: sensorData.GPGGA.altitude,
+        deviceSpeed: sensorData.GPRMC.speed,
+        devicecourse: sensorData.GPRMC.course,
+        deviceVariation: sensorData.GPRMC.variation
+    });
+
+    saveToDb.save(function (err) {
+        if (err) {
+            console.log("Error in saving : " + err);
+        }
+        else {
+            console.log("Saved Data :" + saveToDb)
+        }
+    })
 }
